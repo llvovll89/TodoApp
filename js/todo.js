c97d94f1b35554aa3d2d0a260e68017338e32f62
hojjang <svvvs5579@naver.com>
2022-03-04 17:27:59 +0900

    세번째 커밋

--- js/todo.js
+++ js/todo.js
@@ -12,13 +12,14 @@ function saveToDos() {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  
   li.remove();
+  toDos =  toDos.filter(toDo => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function paintToDo(newTodo) {
   const li = document.createElement("li");
-  li.id = newTodo.id
+  li.id = newTodo.id;
   const span = document.createElement("span");
   span.innerText = newTodo.text;
   const button = document.createElement("button");
@@ -50,4 +51,8 @@ if (savedToDos) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
+}
+
+function sexyFilter() {
+
 }
