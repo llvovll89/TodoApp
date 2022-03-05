08830b8341a56216184204623390da96ae6216ad
hojjang <svvvs5579@naver.com>
2022-03-05 18:44:58 +0900

    middle commit

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+// my api key
+// 403ab7220f45d2c5074c471d2bfd07e2
+
+const API_KEY = "403ab7220f45d2c5074c471d2bfd07e2";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const log = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
